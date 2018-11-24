ae65f6f018f95f9f48969b042cda264756faeae6
benbickel <ben.bickel@fastbridge.org>
2018-11-24 09:32:02 -0600

    initial commit

---
+++ src/main/java/com/conoftherings/conoftherings/ConOfTheRingsApplication.java
@@ -0,0 +1,12 @@
+package com.conoftherings.conoftherings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConOfTheRingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConOfTheRingsApplication.class, args);
+	}
+}
