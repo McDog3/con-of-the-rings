70197f17f74b4a7d4bd2beeeff9df733d2e3a330
benbickel <ben.bickel@fastbridge.org>
2018-12-02 01:19:48 -0600

    Created a REACT navigation bar component utilizing Bootstrap with <nav> tags
    * Includes some demo functionality to change the bar based on whether a user is logged in

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,6 @@
 import LoginControl from "@components/LoginControl";
+import NavBar from "@components/NavBar";
 
 ReactDOM.render(<LoginControl />, document.getElementById('login_container'));
+ReactDOM.render(<NavBar isLoggedIn={false}/>, document.getElementById('cotr_navbar'));
+
